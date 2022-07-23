c9af5449253a2b814091f201fafb7e7db4f2bc87
Denis <chekmarevdenchik5@gmail.com>
2022-07-23 20:26:49 +0700

    001.py is done

--- 001.py
+++ 001.py
@@ -1,5 +1,5 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
 text = 'Муха села набв ваабвренье - вот и все стихотвабворенье'
-new_text = ' '.join(list(filter(lambda x: 'абв' not in x, text.split())))
+new_text = ' '.join(filter(lambda x: 'абв' not in x, text.split()))
 print(new_text)
